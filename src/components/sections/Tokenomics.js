7fe7adb1899f12340c98602dea215936cfc9fcda
creativeorange3 <creativeorange3@gmail.com>
2021-12-01 23:54:40 +0100

    Add: rounding buttons, adding whitelist presale in tokenomics

--- src/components/sections/Tokenomics.js
+++ src/components/sections/Tokenomics.js
@@ -132,6 +132,7 @@ class Tokenomics extends React.Component {
                   <p>Token name: <span>$RXCG</span></p>
                   <p>Total supply: <span>100 000 000</span></p>
                   <p>Private presale: <span>110 BNB (done)</span></p>
+                  <p>Whitelist presale: <span>400 BNB (done)</span></p>
                   <p>Network: <span>BSC</span></p>
                 </div>
                 <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item"
